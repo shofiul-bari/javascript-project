38aa461818dc6289c76061f16ff40bfd1526d49d
shofiul-bari <shofiulbari11@gmail.com>
2024-07-29 20:54:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function text(){
+    document.getElementById('text').innerHTML='hello I am shofiul bari'
+    document.getElementById('bton').innerHTML='FAHIM'
+   // document.getElementById('bton').style.backgroundColor = '#000000'
+ 
+   
+   
+}
+
+function liked(){
+    document.getElementById('like').innerHTML='<i class="fa-solid fa-thumbs-up" style="color: #000000fd; background-color: transparent;"></i> liked'
+   
+   
+}
+
+function disliked(){
+    document.getElementById('dislike').innerHTML='<i class="fa-solid fa-thumbs-down" style="color: #000000fd; background-color: transparent;"Z ></i> Disliked'
+   
+   
+}
